edf60524fe5a74f41a4a071752b5ccea8ad98227
何晓辉 <cangtian02@163.com>
2019-09-24 08:54:34 +0800

    用户主页列表-上拉加载下拉刷新

--- src/view/userHome/userHome.js
+++ src/view/userHome/userHome.js
@@ -2,36 +2,15 @@ import React from 'react';
 import './userHome.css';
 import UserBlock from '../../viewComponents/userBlock/userBlock';
 import TabTitle from '../../viewComponents/tabTitle/tabTitle';
-import NestItem from '../../viewComponents/nestItem/nestItem';
-import ArticleItem from '../../viewComponents/articleItem/articleItem';
-import img from '../submitNest/1.jpg';
-
-const nestItem = {
-  id: 1,
-  img: img,
-  name: '小窝名字啊啊啊小窝名字啊啊啊',
-  userIcon: img,
-  userName: '小明设计师',
-  lable: ['简约', '极客范', '简约', '极客范'],
-  praise: 30,
-};
-
-const articleItem = {
-  id: 1,
-  img: [],
-  name: '文章名字啊啊啊小窝名字啊啊啊',
-  info: '文章内容啊啊啊小窝名字啊啊啊文章内容啊啊啊小',
-  praise: 30,
-  userIcon: img,
-  userName: '小明设计师',
-};
+import NestList from './nestList/nestList';
+import ArticleList from './articleList/articleList';
 
 class Userhome extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      titleTabIndex: 0,
+      tabIndex: 0,
       userInfo: {
         id: 1,
         name: '名字啦啦啦',
@@ -44,8 +23,6 @@ class Userhome extends React.Component {
         lable: ['简约', '90后'],
         info: ''
       },
-      nestItem: [nestItem],
-      articleItem: [articleItem],
     }
   }
 
@@ -53,42 +30,8 @@ class Userhome extends React.Component {
 
   }
 
-  getNolistContent(str) {
-    return <div className="uh-item-nolist">{str}</div>;
-  }
-
-  handleTabTitleClick(titleTabIndex) {
-    this.setState({ titleTabIndex });
-  }
-
-  getNestItemDom(item, str) {
-    if (item.length === 0) return this.getNolistContent(str);
-
-    let arr = [];
-    item.forEach((val, i) => {
-      arr.push(<NestItem val={val} key={i} hiddenUser={true} history={this.props.history} index={i} />);
-    });
-
-    return arr;
-  }
-
-  getArticleItem(item, str) {
-    if (item.length === 0) return this.getNolistContent(str);
-
-    let arr = [];
-    item.forEach((val, i) => {
-      arr.push(<ArticleItem val={val} key={i} hiddenUser={true} history={this.props.history} index={i} />);
-    });
-
-    return arr;
-  }
-
-  getNestContent() {
-    return this.getNestItemDom(this.state.nestItem, 'TA还没有分享小窝哦~');
-  }
-
-  getArticleContent() {
-    return this.getArticleItem(this.state.articleItem, 'TA还没有分享文章哦~',);
+  handleTabTitleClick(tabIndex) {
+    this.setState({ tabIndex });
   }
 
   handleFollow(e) {
@@ -99,18 +42,11 @@ class Userhome extends React.Component {
 
   render() {
     return (
-      <div className="userHome">
+      <div className="userHome" id="userHome">
         <UserBlock history={this.props.history} userInfo={this.state.userInfo} handleFollow={e => this.handleFollow(e)}/>
         <TabTitle style={{ margin: '.6rem 0' }} list={['小窝', '文章']} click={i => this.handleTabTitleClick(i)} />
-        <div className="uh-item">
-          {
-            this.state.titleTabIndex === 0
-            ?
-              this.getNestContent()
-            :
-              this.getArticleContent()
-          }
-        </div>
+        <NestList toggle={this.state.tabIndex === 0} />
+        <ArticleList toggle={this.state.tabIndex === 1} />
       </div>
     );
   }
